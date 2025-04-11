6bb16395a085a19e528331afe7c6015cd8b3824b
rufus-gh <135003986+rufus-gh@users.noreply.github.com>
2025-04-11 18:22:19 +1000

    better solving method and output

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var operations = ['+', '-', '*', '/',  '%', '^'];
+var operations = ['+', '-', '*', '/', '%', '**'];
 const input = document.getElementById('number');
+const output = document.querySelector('.output');
 
-input.addEventListener('keydown', function(event) {
+input.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         event.preventDefault();
         calculateResult(input.value);
@@ -17,17 +18,25 @@ function shuffleArray(array) {
 }
 
 function calculateResult(expression) {
-    //var nums = [];
-    //expression.split('').forEach(num => {nums.push(+num)});
     expression = expression.split('');
     keepGoing = true;
+    let attempts = 0;
+    const maxAttempts = 10000;
+
     while (keepGoing) {
         equation = `${expression[0]}${operations[0]}${expression[1]}${operations[1]}${expression[2]}${operations[2]}${expression[3]}`;
         if (eval(equation) == 10) {
-            console.log(`The equation ${equation} = 10`);
+            output.innerText = `The equation ${equation} = 10`.replace('*', '×').replace('/', '÷').replace('**', '^').replace(/(\d+)%(\d+)/g, 'mod($1, $2)');
+            output.style.opacity = 1;
             keepGoing = false;
         }
         operations = shuffleArray(operations);
-        //console.log('checking', equation);
+        expression = shuffleArray(expression);
+        attempts++; // Increment the counter
+        if (attempts >= maxAttempts) {
+            output.innerText = "No solution found after maximum attempts.";
+            output.style.opacity = 1;
+            keepGoing = false;
+        }
     }
 }
